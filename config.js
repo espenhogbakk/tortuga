346df3ea83ae7ec64913a85260268f52d16dcf29
Espen Høgbakk <espen@hogbakk.no>
2014-05-12 22:50:54 +0200

    Fix default base url.

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 var config = {}
-config.base_url = process.env.PIRATEBAY_HOST || 'http:://thepiratebay.se';
+config.base_url = process.env.PIRATEBAY_HOST || 'http://thepiratebay.se';
 
 module.exports = config;
