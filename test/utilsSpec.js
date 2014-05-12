fec20e6123ac1288baa509f2a20649aaca08f50f
Espen Høgbakk <espen@hogbakk.no>
2014-05-12 22:59:18 +0200

    Refactor utils.

--- test/utilsSpec.js
+++ test/utilsSpec.js
@@ -3,12 +3,12 @@ var utils = require("../lib/utils.js");
 
 describe("convertBytesToMegabytes()", function() {
   it("should convert bytes to megabytes", function() {
-    expect(convertBytesToMegabytes(1010827264)).to.equal(964)
+    expect(utils.convertBytesToMegabytes(1010827264)).to.equal(964)
   });
 });
 
 describe("convertMegaBytesToBytes()", function() {
   it("should convert megabytes to bytes", function() {
-    expect(convertMegabytesToBytes(964)).to.equal(1010827264)
+    expect(utils.convertMegabytesToBytes(964)).to.equal(1010827264)
   });
 });
