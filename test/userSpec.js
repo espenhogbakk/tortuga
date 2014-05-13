b36cddaf6ed94aa4802c540aff2eb879f6706071
Espen Høgbakk <espen@hogbakk.no>
2014-05-13 15:51:51 +0200

    Add User class.

---
+++ test/userSpec.js
@@ -0,0 +1,20 @@
+var expect = require('chai').expect;
+
+var config = require('../index').config;
+var User = require('../lib/pirater/user');
+
+describe('User', function() {
+  var user = new User('foo');
+
+  describe('.username', function() {
+    it('should return the username', function() {
+      expect(user.username).to.equal('foo');
+    });
+  });
+
+  describe('.profileUrl', function() {
+    it('should return the profile url', function() {
+      expect(user.profileUrl).to.equal(config.base_url + '/user/foo');
+    });
+  });
+});
