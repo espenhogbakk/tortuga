5eb41d457258e80e4e539a9b09b7014db974ff15
Jit Kanetkar <kanetkarster@gmail.com>
2014-05-17 13:56:39 -0400

    Test functional for 'Leechers', 'Seeders' and 'Uploader'. 'files', 'date', and 'size' values are undefined in torrent objects

--- test/sortTest.js
+++ test/sortTest.js
@@ -1,9 +1,11 @@
 var tortuga = require('./../lib/tortuga.js');
+var expect = require('chai').expect;
+
 function search (search_terms) {
 	tortuga.search(search_terms, function (results) {
-		results.forEach(function (torrent) {
-			console.log(torrent.bytes + ',  ' + torrent.title);
-		});
+		for (var i = 0; i < results.length - 1; i++) {
+			expect(results[i][search_terms.sortType.toLowerCase()]).to.be.at.least(results[i+1][search_terms.sortType.toLowerCase()])
+		};
 	});
 }
-search({query: process.argv[2], sortType: process.argv[3]});
+search({query: process.argv[2], sortType: process.argv[3].toUpperCase() /*, category: process.argv[4].toUpperCase()*/ });
